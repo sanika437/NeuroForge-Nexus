ffcef9a41fcc6b912c5bccdd091634df8e2ef5f6
Sanika Mane <sanikamane141@gmail.com>
2026-06-30 19:56:47 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/infosys/neuroforge/NeuroforgeNexusApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.neuroforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroforgeNexusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuroforgeNexusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infosys/neuroforge/NeuroforgeNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.neuroforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroforgeNexusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
